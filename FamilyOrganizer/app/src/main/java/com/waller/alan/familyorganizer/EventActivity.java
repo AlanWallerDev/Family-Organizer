a477609195a92fc42ada1de0dc3b2e453129cd2b
Alan Waller <zancar615@gmail.com>
2018-04-15 17:36:59 -0700

    added todos

---
+++ FamilyOrganizer/app/src/main/java/com/waller/alan/familyorganizer/EventActivity.java
@@ -0,0 +1,9 @@
+package com.waller.alan.familyorganizer;
+
+/**
+ * Created by Alan on 4/15/2018.
+ */
+//todo: create an activity that allows the user to create events, and see events created by themselves or people on their contacts
+    //optionally, maybe allow the user to add events to their contacts?
+public class EventActivity {
+}
---
+++ FamilyOrganizer/app/src/main/java/com/waller/alan/familyorganizer/ListActivity.java
@@ -0,0 +1,8 @@
+package com.waller.alan.familyorganizer;
+
+/**
+ * Created by Alan on 4/15/2018.
+ */
+//todo: create an activity that allows the user to create lists, and see lists that have been created by themselves or people on their contacts
+public class ListActivity {
+}
