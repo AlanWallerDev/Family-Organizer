9aab353f2d16cc2f8e9958feee53c90994df3391
Alan Waller <zancar615@gmail.com>
2018-04-15 17:28:00 -0700

    message menu work begin

---
+++ FamilyOrganizer/app/src/main/java/com/waller/alan/familyorganizer/MessageMenu.java
@@ -0,0 +1,13 @@
+package com.waller.alan.familyorganizer;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Alan on 4/15/2018.
+ */
+//todo: create activity which allows you to select from your contacts and when contact is selected
+    // open a message activity that pulls all messages to and from that contact
+    //will need to access database to pull all the names of the contacts "owned" by this user
+    //and display them in a menu
+public class MessageMenu extends AppCompatActivity {
+}
